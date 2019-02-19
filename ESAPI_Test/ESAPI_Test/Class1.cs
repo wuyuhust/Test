d75cc535ac8f38fc294a8e084c6ca50df0b284c3
Yu Wu <yu.wu@wustl.edu>
2019-02-19 16:22:12 -0600

    hello ESAPI

---
+++ ESAPI_Test/ESAPI_Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using VMS.TPS.Common.Model.API;
+
+namespace VMS.TPS
+
+{
+    public class Script
+    {
+        public Script()
+        {
+        }
+
+        public void Execute(ScriptContext context, Window window)
+        {
+            string name = context.Patient.LastName + ", " + context.Patient.FirstName;
+            MessageBox.Show(name);
+        }
+    }
+}
